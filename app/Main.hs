d7cd224f560de32ee18a8f57474d21c2106c26c7
Sandy Maguire <sandy@sandymaguire.me>
2016-03-19 04:13:11 -0700

    collision detection

--- app/Main.hs
+++ app/Main.hs
@@ -13,12 +13,19 @@ type Prop = Prop' ()
 
 movement :: Signal Prop
 movement = foldp update (filled red $ rect origin 20 20) $
-    (,) <$> elapsed <*> KB.wasd
+    (,) <$> elapsed <*> KB.arrows
   where
-    update (dt, dpos) p = move (scaleRel dt $ dpos * 300) p
+    update (dt, dir) p =
+        let dpos = scaleRel dt $ dir * 300
+         in if null $ sweepLine otherBlock (center p) dpos
+               then move dpos p
+               else p
+
+otherBlock :: [Prop]
+otherBlock = return . filled white $ rect (mkPos 200 200) 40 40
 
 mainSig :: Signal [Prop]
-mainSig = return <$> movement
+mainSig = (: otherBlock) <$> movement
 
 main = run config mainSig
 
--- src/Game/Sequoia/Utils.hs
+++ src/Game/Sequoia/Utils.hs
@@ -1,10 +1,15 @@
 module Game.Sequoia.Utils
     ( mapT
+    , showTrace
     ) where
 
 import Control.Arrow ((***))
 import Control.Monad (join)
+import Debug.Trace
 
 mapT :: (a -> b) -> (a, a) -> (b, b)
 mapT = join (***)
+
+showTrace :: Show a => a -> a
+showTrace = trace =<< show
 

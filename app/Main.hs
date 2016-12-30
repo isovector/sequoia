bfa26c62b812ac1871cb21f71505aa26e8f5a9ce
Sandy Maguire <sandy@sandymaguire.me>
2016-12-29 21:42:20 -0800

    what is UP skeletal animations

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,56 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Main where
 
-import Control.Eff
-import Control.Eff.Reader.Lazy
+import Data.String.Conv (toS)
+import Control.Lens
+import Data.Spriter.Types
+import Data.Spriter.Skeleton
+import Data.Aeson (decode)
 import Control.Monad
+import Control.Monad.IO.Class (liftIO)
 import Game.Sequoia
 import Game.Sequoia.Color
 import Game.Sequoia.Keyboard
+import Data.Scientific (toRealFloat)
 
 type Prop = Prop' ()
-
-square :: ( Member (Reader (Behavior Time)) r
-          , Member (Reader (Behavior [Key])) r
-          )
-       => [Prop]
-       -> Eff r (Now (Behavior Prop, (Prop -> Prop) -> IO ()))
-square walls = do
-    (clock :: Behavior Time) <- ask
-    (keys :: Behavior [Key]) <- ask
-
-    return $ do
-        foldmp (group [ filled red $ rect origin 50 50
-                      , filled red $ circle (mkPos 0 $ -25) 25
-                      ]) $ \sq -> do
-            dt   <- sample clock
-            dpos <- sample $ arrows keys
-            return $ tryMove walls [] sq (scaleRel (300 * dt) dpos)
 
 magic :: Prop -> Engine -> Now (Behavior Prop)
 magic baller _ = do
     clock    <- getClock
-    keyboard <- getKeyboard
+    Just schema <- liftIO $ decode . toS <$> readFile "/home/bootstrap/Projects/bones/basic-anim.scon"
 
     return $ do
         now <- sample $ totalTime clock
-        return $ group [rotate now $ scale (abs $ sin now * 2) baller]
+        return $ doAnimation schema (((round $ now * 100) `mod` 290) + 10)
+
+
+makeBones :: Schema -> [Prop]
+makeBones schema = toProp <$> schema ^. schemaEntity._head.entityObjInfo
+  where
+    toProp Bone{..} = traced white
+                    $ polygon origin
+                      [ rel 0 (toRealFloat $ _boneHeight / 2)
+                      , rel (toRealFloat _boneWidth) 0
+                      , rel 0 (toRealFloat $ (-_boneHeight) / 2)
+                      ]
+
+doAnimation :: Schema -> Int -> Prop
+doAnimation schema frame =
+  let bones = animate (head $ schema ^. schemaEntity._head.entityAnimation)
+                      frame
+      drawBone ResultBone{..} = move (rel _rbX (-_rbY))
+                              . rotate (-_rbAngle)
+   in case bones of
+        Just x -> group . fmap (uncurry drawBone)
+                        $ zip x (makeBones schema)
+        Nothing -> traced red $ rect origin 10 10
+
 
 main :: IO ()
 main = withTexture "app/baller.png" $ \baller ->

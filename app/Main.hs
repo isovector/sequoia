b30f0c4b7223390e367f98a3fc918c2e2a67e3eb
Sandy Maguire <sandy@sandymaguire.me>
2017-01-02 15:36:39 -0800

    sprite animation

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@
 
 module Main where
 
+import Data.Ord (comparing)
+import Data.List (sortBy)
+import Data.Monoid ((<>))
 import Data.String.Conv (toS)
 import Control.Lens
 import Data.Spriter.Types
@@ -21,18 +24,26 @@ import Data.Scientific (toRealFloat)
 magic :: Engine -> Now (Behavior Element)
 magic _ = do
     clock <- getClock
-    Just json  <- liftIO $ decode . toS <$> readFile "/home/bootstrap/Projects/bones/basic-anim.scon"
+    Just json  <- liftIO $ decode . toS <$> readFile "/home/bootstrap/Projects/bones/skinned.scon"
     let schema' = fromJSON json
     liftIO $ print schema'
     let Success schema = schema'
 
     return $ do
         now <- sample $ totalTime clock
-        let skel = scale 0.5 $ doAnimation schema $ ((round $ now * 100) `mod` 300)
+        let skel = scale 0.5 $ doBonewise schema $ ((round $ now * 100) `mod` 300)
+            anim = scale 0.5 $ doSpritewise schema $ ((round $ now * 100) `mod` 300)
         return $ centeredCollage 640 480
-               [ move (V2 0 400) $ skel
-               , sprite "app/baller.png"
+               [ move (V2 0 (-100)) anim
+               , move (V2 0 (-100)) skel
                ]
+
+makeSprites :: Schema -> [(Form, File)]
+makeSprites schema = toProp
+                 <$> schema ^. schemaFolder._head.folderFile
+  where
+    toProp f@File{..} = (sprite $ "/home/bootstrap/spriter-test/" <> _fileName, f)
+
 
 
 makeBones :: Schema -> [Form]
@@ -45,15 +56,27 @@ makeBones schema = toProp <$> schema ^. schemaEntity._head.entityObjInfo
                       , V2 0 (toRealFloat $ (-_boneHeight) / 2)
                       ]
 
-doAnimation :: Schema -> Int -> Form
-doAnimation schema frame =
+doSpritewise :: Schema -> Int -> Form
+doSpritewise schema frame =
+  let bones = animate (head $ schema ^. schemaEntity._head.entityAnimation)
+                      frame
+      drawBone ResultBone{..} (form, _) =
+        move (V2 _rbX $ -_rbY) . rotate (-_rbAngle)
+                               $ form
+   in case bones of
+        Just x -> group . fmap (uncurry drawBone)
+                        $ zip (sortBy (comparing $ (fmap . fmap) _boneObjFile _rbObj) $ filter (not . isBone) x) (makeSprites schema)
+        Nothing -> traced' red $ rect 10 10
+
+doBonewise :: Schema -> Int -> Form
+doBonewise schema frame =
   let bones = animate (head $ schema ^. schemaEntity._head.entityAnimation)
                       frame
       drawBone ResultBone{..} = move (V2 _rbX $ -_rbY)
                               . rotate (-_rbAngle)
    in case bones of
         Just x -> group . fmap (uncurry drawBone)
-                        $ zip x (makeBones schema)
+                        $ zip (filter isBone x) (makeBones schema)
         Nothing -> traced' red $ rect 10 10
 
 
